81328b4330ec0ee601a787443a37162a5f3f8e87
YouthBaby <540969500@qq.com>
2020-08-16 23:47:31 +0800

    第八周作业

---
+++ Week_08/hammingWeight.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+  let count = 0;
+  if (n === 0) return count;
+  while (n) {
+    count++;
+    n = n & (n - 1);
+  }
+  return count;
+};
